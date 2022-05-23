ac8f5afbf294d3de84699be825ec26f5d9ba3a0a
KawinM23 <kawin.m235@gmail.com>
2022-05-23 17:59:54 +0700

    Add Firebase config

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNwibrAs3vouz0LYdWKSJ5R9_cL9nQhHQ",
+  authDomain: "embedded-lab-project.firebaseapp.com",
+  projectId: "embedded-lab-project",
+  storageBucket: "embedded-lab-project.appspot.com",
+  messagingSenderId: "414851825213",
+  appId: "1:414851825213:web:11c113b3999935fb7e7454"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
